520dae80455dfafc45a06701edd67d54ccf576ec
WTDula <wtdula@gmail.com>
2022-05-12 14:22:51 -0500

    created searchbar component, installed google package, installed axios, tested environment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar/SearchBar';
+//import './App.css';
 
 function App() {
+
+  const [games, setGames] = useState([])
+
+  const getGames = async() => {
+    let request = await axios.get("https://localhost:7260/api/games")
+    setGames(request.data)
+  }
+
+  useEffect(() => {
+    getGames()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
+
     </div>
   );
 }
---
+++ src/SearchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+
+
+const SearchBar = (props) => {
+    return ( 
+        <div>
+            Search Bar here
+        </div>
+     );
+}
+ 
+export default SearchBar;
